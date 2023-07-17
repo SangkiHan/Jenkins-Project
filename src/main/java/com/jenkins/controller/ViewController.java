11fd4125e81ffc3e8c5ae9490bc763def55e2e01
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-07-17 17:27:14 +0900

    Start Jenkins

---
+++ src/main/java/com/jenkins/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+	
+	@GetMapping("main")
+	public String main() {
+		return "main/main";
+	}
+
+}
